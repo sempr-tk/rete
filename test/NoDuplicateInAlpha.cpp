32d4a76bad0bc13b2407e7f0fe5459a0f8f4faa8
Nils <nils.niemann@dfki.de>
2018-08-15 10:12:55 +0200

    small test against duplicate WMEs in alpha memories

---
+++ test/NoDuplicateInAlpha.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+#include "../include/Rete.hpp"
+
+using namespace rete;
+
+/**
+    Multiple additions of the same (or equal) WME should not result in multiple entries in the
+    alpha memories.
+*/
+int main()
+{
+    Network net;
+
+    /*
+        (predicate == "self"?) ____
+                                   ` (unconditional join)
+        (predicate == "color"?) ---'
+    */
+
+    auto root = net.getRoot();
+    TripleAlpha::Ptr a1(new TripleAlpha(Triple::PREDICATE, "self")); root->addChild(a1);
+    TripleAlpha::Ptr b1(new TripleAlpha(Triple::PREDICATE, "color")); root->addChild(b1);
+    a1->initAlphaMemory();
+    b1->initAlphaMemory();
+
+    AlphaBetaAdapter::Ptr ab(new AlphaBetaAdapter(a1->getAlphaMemory()));
+    a1->getAlphaMemory()->addChild(ab);
+
+    JoinNode::Ptr j1(new JoinNode(ab->getBetaMemory(), b1->getAlphaMemory()));
+    ab->getBetaMemory()->addChild(j1); b1->getAlphaMemory()->addChild(j1);
+
+
+    // add knowledge
+    Triple::Ptr t1(new Triple("B1", "self", "B1"));
+    Triple::Ptr t2a(new Triple("B1", "color", "red"));
+    Triple::Ptr t2b(new Triple("B1", "color", "red"));
+
+    // duplicate: same instance
+    root->activate(t1, rete::ASSERT);
+    root->activate(t1, rete::ASSERT);
+    if (a1->getAlphaMemory()->size() != 1) return 1;
+
+    root->activate(t2a, rete::ASSERT);
+    root->activate(t2b, rete::ASSERT);
+    if (b1->getAlphaMemory()->size() != 1) return 2;
+
+
+    return 0;
+}

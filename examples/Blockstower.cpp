5bb50f29055f6d004b0fd511f17e316ad5023b72
Nils Niemann <nils.niemann@dfki.de>
2019-03-05 13:32:31 +0100

    add two more examples

---
+++ examples/Blockstower.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+
+#include "../rete-rdf/ReteRDF.hpp"
+
+#include "../rete-reasoner/Reasoner.hpp"
+#include "../rete-reasoner/RuleParser.hpp"
+#include "../rete-reasoner/AssertedEvidence.hpp"
+
+
+using namespace rete;
+
+
+void save(Network& net, const std::string& filename)
+{
+    std::ofstream out(filename);
+    out << net.toDot();
+    out.close();
+}
+
+Triple::Ptr block(const std::string& name)
+{
+    Triple::Ptr t(new Triple(name, "<type>", "<block>"));
+    return t;
+}
+
+Triple::Ptr on(const std::string& n1, const std::string& n2)
+{
+    Triple::Ptr t(new Triple(n1, "<on>", n2));
+    return t;
+}
+
+
+int main()
+{
+    RuleParser p;
+    Reasoner reasoner;
+
+
+    p.parseRules(
+    "[AtTheBottom: (?b <type> <block>), noValue (?b <on> ?c) -> (?b <level> 0)]"
+    "[OnAnother:   (?a <on> ?b), (?b <level> ?lb), sum(?la ?lb 1) -> (?a <level> ?la)]",
+        reasoner.net()
+    );
+
+    Evidence::Ptr evidence(new AssertedEvidence("fact-group-1"));
+
+    reasoner.addEvidence(block("a0"), evidence);
+    reasoner.addEvidence(block("a1"), evidence);
+    reasoner.addEvidence(block("a2"), evidence);
+    reasoner.addEvidence(block("a3"), evidence);
+    reasoner.addEvidence(block("a4"), evidence);
+
+    reasoner.addEvidence(block("b0"), evidence);
+    reasoner.addEvidence(block("b1"), evidence);
+    reasoner.addEvidence(block("b2"), evidence);
+
+    reasoner.addEvidence(on("a1", "a0"), evidence);
+    reasoner.addEvidence(on("a2", "a1"), evidence);
+    reasoner.addEvidence(on("a3", "a2"), evidence);
+    reasoner.addEvidence(on("a4", "a3"), evidence);
+
+    reasoner.addEvidence(on("b1", "b0"), evidence);
+    reasoner.addEvidence(on("b2", "b1"), evidence);
+
+
+    reasoner.performInference();
+
+    save(reasoner.net(), "blockstower.dot");
+
+    return 0;
+}
---
+++ examples/Blocksworld.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <fstream>
+
+#include "../rete-rdf/ReteRDF.hpp"
+#include "../rete-reasoner/AssertedEvidence.hpp"
+
+#include "../rete-reasoner/Reasoner.hpp"
+#include "../rete-reasoner/RuleParser.hpp"
+
+
+using namespace rete;
+
+
+void save(Network& net, const std::string& filename)
+{
+    std::ofstream out(filename);
+    out << net.toDot();
+    out.close();
+}
+
+Triple::Ptr block(const std::string& name)
+{
+    Triple::Ptr t(new Triple(name, "<type>", "<block>"));
+    return t;
+}
+
+Triple::Ptr red(const std::string& name)
+{
+    Triple::Ptr t(new Triple(name, "<color>", "<red>"));
+    return t;
+}
+
+Triple::Ptr leftof(const std::string& n1, const std::string& n2)
+{
+    Triple::Ptr t(new Triple(n1, "<left-of>", n2));
+    return t;
+}
+
+Triple::Ptr on(const std::string& n1, const std::string& n2)
+{
+    Triple::Ptr t(new Triple(n1, "<on>", n2));
+    return t;
+}
+
+Triple::Ptr size(const std::string& name, float size)
+{
+    Triple::Ptr t(new Triple(name, "<edge-size>", std::to_string(size)));
+    return t;
+}
+
+int main()
+{
+    RuleParser p;
+    Reasoner reasoner;
+
+
+    p.parseRules(
+"[OnARedBlock: (?b1 <on> ?b2), (?b2 <color> <red>) -> (?b1 <on-red-block> ?b2)]\n"
+"[LeftOfANotRedBlock:"
+    "(?b1 <left-of> ?b2), noValue (?b2 <color> <red>) -> (?b1 <left-of-not-red-block> ?b2)]\n"
+"[BlockVolume:"
+    "(?b <type> <block>), (?b <edge-size> ?len), mul(?volume ?len ?len ?len)"
+    "-> (?b <volume> ?volume)]",
+        reasoner.net()
+    );
+
+    Evidence::Ptr evidence(new AssertedEvidence("fact-group-1"));
+
+    reasoner.addEvidence(block("a0"), evidence);
+    reasoner.addEvidence(block("a1"), evidence);
+    reasoner.addEvidence(block("b0"), evidence);
+    reasoner.addEvidence(block("b1"), evidence);
+    reasoner.addEvidence(block("c"), evidence);
+    reasoner.addEvidence(block("d"), evidence);
+
+    reasoner.addEvidence(on("a1", "a0"), evidence);
+    reasoner.addEvidence(on("b0", "b1"), evidence);
+
+    reasoner.addEvidence(red("a0"), evidence);
+    reasoner.addEvidence(red("c"), evidence);
+
+    reasoner.addEvidence(leftof("c", "a0"), evidence);
+    reasoner.addEvidence(leftof("d", "c"), evidence);
+    reasoner.addEvidence(leftof("d", "b0"), evidence);
+
+    reasoner.addEvidence(size("a0", 1.451), evidence);
+    reasoner.addEvidence(size("b0", 2), evidence);
+    reasoner.addEvidence(size("c", 3), evidence);
+    reasoner.addEvidence(size("d", 4), evidence);
+
+
+    reasoner.performInference();
+
+    save(reasoner.net(), "blocksworld.dot");
+
+    return 0;
+}

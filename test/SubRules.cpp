7a0eb4215f47f4ed18da5c4e9adf7368778885ac
Nils Niemann <nils.niemann@dfki.de>
2021-04-07 17:26:08 +0200

    test for else branches

--- test/SubRules.cpp
+++ test/SubRules.cpp
@@ -121,7 +121,90 @@ bool parse_multi_depth_sub_rule()
     return true;
 }
 
+bool parsing_single_rule_with_else_should_fail()
+{
+    RuleParser p;
+    Reasoner reasoner;
+    try {
+        auto rules = p.parseRules(
+            "[(?a <foo> ?b) -> (?a <bar> ?b) else (?a <baz> ?b)]",
+            reasoner.net()
+        );
+    } catch (RuleConstructionException& e) {
+        std::cout << e.what() << std::endl;
+        return true;
+    }
 
+    std::ofstream(__func__ + std::string(".dot")) << reasoner.net().toDot();
+    return false;
+}
+
+bool parsing_subrule_with_else()
+{
+    RuleParser p;
+    Reasoner reasoner;
+    auto rules = p.parseRules(
+        "[foo: (?a <foo> ?b)\n"
+        " [bar: (?a <bar> ?b) -> (?a <foobar> ?b) else (?a <foobaz> ?b)]\n"
+        "]",
+        reasoner.net()
+    );
+
+    std::ofstream(__func__ + std::string(".dot")) << reasoner.net().toDot();
+    return true;
+}
+
+bool subrule_with_else_output_correct()
+{
+    RuleParser p;
+    Reasoner reasoner;
+    auto rules = p.parseRules(
+        "[foo: (?a <foo> ?b)\n"
+        " [bar: (?a <bar> ?b) -> (?a <foobar> ?b) else (?a <foobaz> ?b)]\n"
+        "]\n"
+        ,
+        reasoner.net()
+    );
+    std::ofstream(__func__ + std::string("0.dot")) << reasoner.net().toDot();
+
+    auto dataRules = p.parseRules(
+        "[data: true() -> (<A> <foo> <B>)]",
+        reasoner.net()
+    );
+
+    reasoner.performInference();
+    std::ofstream(__func__ + std::string("1.dot")) << reasoner.net().toDot();
+
+    if (!containsTriple(reasoner, "<A>", "<foobaz>", "<B>") ||
+         containsTriple(reasoner, "<A>", "<foobar>", "<B>"))
+        return false;
+
+    auto dataRules2 = p.parseRules(
+        "[data2: true() -> (<A> <bar> <B>)]",
+        reasoner.net()
+    );
+
+    reasoner.performInference();
+    std::ofstream(__func__ + std::string("2.dot")) << reasoner.net().toDot();
+
+    if (containsTriple(reasoner, "<A>", "<foobaz>", "<B>") ||
+        !containsTriple(reasoner, "<A>", "<foobar>", "<B>"))
+        return false;
+
+    return true;
+}
+
+
+/*
+{
+    RuleParser p;
+    Reasoner reasoner;
+    auto rules = p.parseRules(
+        "",
+        reasoner.net()
+    );
+}
+*/
 #define TEST(function) \
     { \
         bool ok; \
@@ -145,5 +228,8 @@ int main()
     TEST(parse_single_sub_plus_concrete_rule);
     TEST(parse_multi_sub_rule);
     TEST(parse_multi_depth_sub_rule);
+    TEST(parsing_single_rule_with_else_should_fail);
+    TEST(parsing_subrule_with_else);
+    TEST(subrule_with_else_output_correct);
     return failed;
 }
